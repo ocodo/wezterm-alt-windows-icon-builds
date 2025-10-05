cb3977ed11ae670ece8726cccdfed25516d8fc08
noidilin <linganinja.0120@gmail.com>
2025-10-05 18:28:39 +0800

    fix: support auto-pr script

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,8 @@
+# this tells the auto-pr script which upstream repository to create pull requests against when automated updates are found by checkver.ps1.
+
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "noidilin/wezterm-alt-icon:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
