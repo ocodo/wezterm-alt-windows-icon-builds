dc50bd32444a380adc5e76f6d1fee2ef75b2441f
noidilin <linganinja.0120@gmail.com>
2025-10-15 18:52:52 +0800

    ci: limit the scoop-ci.yml test inside bucket dir

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,2 +1,7 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
+
+# Determine the path to the bucket directory
+$bucketPath = Resolve-Path "$PSScriptRoot\..\bucket"
+
+# Import the core test script and pass the bucket path
+. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1" -BucketPath $bucketPath
