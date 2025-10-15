bf566f1fc3b1777a84357554fb86c69953c085d0
noidilin <linganinja.0120@gmail.com>
2025-10-15 19:01:19 +0800

    fix: ci path resolution duplication

--- Scoop-Bucket.Tests.ps1
+++ Scoop-Bucket.Tests.ps1
@@ -1,7 +1,7 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 
 # Determine the path to the bucket directory
-$bucketPath = Resolve-Path "$PSScriptRoot\..\bucket"
+$bucketPath = Resolve-Path "$PSScriptRoot\bucket"
 
 # Import the core test script and pass the bucket path
 . "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1" -BucketPath $bucketPath
